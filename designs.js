24b3eab5089524c320453c6aa408fbe84d86b956
hamjahb <aljahbli1@yahoo.com>
2019-04-18 14:42:46 +0300

    added DOM elements

--- designs.js
+++ designs.js
@@ -1,10 +1,26 @@
+//ver 0.1
+
 // Select color input
+//this gets the color value from the selector
+var color = document.getElementById("colorPicker").value;
+
 // Select size input
+
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+function makeGrid(event) {
 
 // Your code goes here!
+//to get value of Height and width 
+var height = document.getElementById("inputHeight").value;
+var width = document.getElementById("inputWidth").value;
 
+// listen for submit event
+var form = document.getElementById("form");
+form.addEventListener("submit");
+
+// create a pixel square
+var pixel = document.createElement("div")
 }
+
