252df4bd07ffd4f9db0c6bbd0e8071076d92464c
hamjahb <aljahbli1@yahoo.com>
2019-04-21 20:40:19 +0300

    edited semi-colons and changed var to const

--- designs.js
+++ designs.js
@@ -1,44 +1,44 @@
-//ver 1.0
+//ver 1.1
 
 //adds color to selected cells
 function colorCell(event) {
 
     // Select color input and change cell style
-    var color = document.getElementById("colorPicker").value;
+    const color = document.getElementById("colorPicker").value;
     event.target.style.backgroundColor = color;
-};
+}
 
 //make grid function
 function makeGrid() {
 
     // Select size input
-    var height = document.getElementById("inputHeight").value;
-    var width = document.getElementById("inputWidth").value;
-    var table = document.getElementById("pixelCanvas");
+    const height = document.getElementById("inputHeight").value;
+    const width = document.getElementById("inputWidth").value;
+    const table = document.getElementById("pixelCanvas");
 
     //reset table on new submit
     table.innerHTML = "";
 
     //create new rows
     for (row = 1; row <= height; row++ ) {
-        var newRow = document.createElement("tr");
+        const newRow = document.createElement("tr");
         table.appendChild(newRow);
 
         //create columns in rows
         for (column = 1; column <= width; column++) {
-            var newColumn = document.createElement("td");
+            const newColumn = document.createElement("td");
             newRow.appendChild(newColumn);
         };
         //add event listner to each cell
         newRow.addEventListener("click", colorCell);
     }; 
-};
+}
 
 
 /* listen for submit event and build grid
 When size is submitted by the user, call makeGrid()*/
-var gridSize = document.getElementById("sizePicker");
+const gridSize = document.getElementById("sizePicker");
 gridSize.addEventListener("submit", function (event) {
     event.preventDefault();
     makeGrid();
-});
+})
